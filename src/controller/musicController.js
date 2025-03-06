cfe56127b1d941cb6e8507d2a3877845f2f0328d
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-03-06 09:16:53 -0300

    fix: server

--- src/controller/musicController.js
+++ src/controller/musicController.js
@@ -1,33 +1,45 @@
 import MusicModel from '../models/musicModel.js';
 
-class MusicController{
+class MusicController {
     getAll = (req, res) => {
         const songs = MusicModel.getAll();
-        res.json(songs)
+        res.json(songs);
     };
-    create = ({ body: { title, author }}, res) => {
-        if(!title){
-            res.status(400).json({message: 'Por favor, insira um título para a música.'});
-        }else if(!author){
-            res.status(400).json({message: 'Por favor, insira um autor para a música.'});
-        };
+
+    getSongById = (req, res) => {
+        const { id } = req.params;
+        const song = MusicModel.getById(id);
+        if (!song) {
+            return res.status(404).json({ erro: "Música não encontrada" });
+        }
+        res.json(song);
+    };
+
+    create = ({ body: { title, author } }, res) => {
+        if (!title) {
+            res.status(400).json({ message: 'Por favor, insira um título para a música.' });
+        } else if (!author) {
+            res.status(400).json({ message: 'Por favor, insira um autor para a música.' });
+        }
         const newSong = MusicModel.create(title, author);
         res.status(201).json(newSong);
     };
+
     update = ({ params: { id }, body: { title, author } }, res) => {
         const musicaAtualizada = MusicModel.update(id, title, author);
         if (!musicaAtualizada) {
             return res.status(404).json({ erro: "Música não encontrada" });
-        };
+        }
         res.json(musicaAtualizada);
     };
+
     delete = ({ params: { id } }, res) => {
-        const success = MusicModel;
-        if(!success) {
+        const success = MusicModel.delete(id);
+        if (!success) {
             return res.status(404).json({ erro: "Música não encontrada" });
-        };
-        res.status(204).json({message: "Música deletada com sucesso"});
+        }
+        res.status(204).json({ message: "Música deletada com sucesso" });
     };
-};
+}
 
 export default new MusicController();
--- src/routes/musicRoutes.js
+++ src/routes/musicRoutes.js
@@ -1,21 +1,21 @@
 import express from 'express';
-import MusicControllers from '../controllermusicController.js';
+import MusicControllers from '../controller/musicController.js';
 
 const router = express.Router();
 
 // Definindo as rotas para listagem de todos os Músicas
-router.get('/', MusicControllers.getSongs);
+router.get('/', MusicControllers.getAll);
 
 // Definindo as rotas para listagem de um Música por id
 router.get('/:id', MusicControllers.getSongById);
 
 //criação de um novo Música
-router.post('/', MusicControllers.createSong);
+router.post('/', MusicControllers.create);
 
 //atualização de um Música
-router.put('/:id', MusicControllers.updateSong);
+router.put('/:id', MusicControllers.update);
 
 //deletar um Música
-router.delete('/:id', MusicControllers.deleteSong);
+router.delete('/:id', MusicControllers.delete);
 
 export default router;
